06eee9223e0e297b8246f7ef0d33fa71c8c32848
黒田建彰 <tatsuaki@orange.ocn.ne.jp>
2018-05-02 00:07:28 +0900

    波の表示高さ調整

--- vHIT96da/ViewController.swift
+++ vHIT96da/ViewController.swift
@@ -923,8 +923,8 @@ class ViewController: UIViewController, MFMailComposeViewControllerDelegate{
         for n in 1...(pointCount) {
             if num + n < vHITouter.count {
                 let px = CGFloat(dx * n)
-                let py = CGFloat(vHITouter[num + n]/2 + 120)
-                let py2 = CGFloat(vHITeye[num + n]/2 + 60)
+                let py = CGFloat(vHITouter[num + n]/3 + 120)
+                let py2 = CGFloat(vHITeye[num + n]/3 + 60)
                 let point = CGPoint(x: px, y: py)
                 let point2 = CGPoint(x: px, y: py2)
                 pointList.append(point)
